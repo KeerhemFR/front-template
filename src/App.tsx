fa2da4740e65d3e1b0731e7da56795cdd5c21c24
Guillaume Keerhem <guillaumekeerhem@MacBook-Pro-de-Guillaume.local>
2022-03-24 17:09:02 +0100

    Initialized the components call

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import Main from './components/Main';
 
 function App() {
-  return <div className="App">Welcome to new app</div>;
+  return <Main />;
 }
 
 export default App;
