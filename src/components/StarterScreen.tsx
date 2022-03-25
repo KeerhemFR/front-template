a73bbb3a170b41aa8eb953a3c0834087750e1abe
Guillaume Keerhem <guillaumekeerhem@MacBook-Pro-de-Guillaume.local>
2022-03-25 14:20:14 +0100

    Add some JSDoc

--- src/components/StarterScreen.tsx
+++ src/components/StarterScreen.tsx
@@ -2,6 +2,11 @@ import React from 'react';
 import BgPicture from '/images/visuel-hp-soin-desktop.png';
 import SStarterScreen from './SStarterScreen';
 
+/**
+ * Screen displayed at the start of the experience
+ * @prop {func} start set if the start screen is displayed or not
+ * @return {JSX} Screen with a button to start the experience
+ */
 interface StarterScreenProps {
   start: (displayStart: boolean) => void;
 }
