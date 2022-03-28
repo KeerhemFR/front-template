eaf0b65056902dd31d7dea968303d6d8305e4a32
Guillaume Keerhem <guillaumekeerhem@mbpdeguillaume.lille.fr.ibm.com>
2022-03-28 16:27:51 +0200

    Changed the imports order to fit best practice

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Main } from './components/Main';
 import { BrowserRouter } from 'react-router-dom';
+import { Main } from './components/Main';
 
 export const App = () => {
   return (
--- src/components/StarterScreen.tsx
+++ src/components/StarterScreen.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import BgPicture from '/images/visuel-hp-soin-desktop.png';
 import SStarterScreen from './SStarterScreen';
-import { Link } from 'react-router-dom';
 
 /**
  * Screen displayed at the start of the experience
