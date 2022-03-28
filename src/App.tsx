ab557154f6e2508462add374acf5a238a8cb1cf5
Guillaume Keerhem <guillaumekeerhem@mbpdeguillaume.lille.fr.ibm.com>
2022-03-28 16:15:09 +0200

    Changed App call from default to classic

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,10 @@ import React from 'react';
 import { Main } from './components/Main';
 import { BrowserRouter } from 'react-router-dom';
 
-function App() {
+export const App = () => {
   return (
     <BrowserRouter>
       <Main />
     </BrowserRouter>
   );
-}
-
-export default App;
+};
