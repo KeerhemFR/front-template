788d794ac00bae06e8336cb5cbb51ee20a5c551e
Guillaume Keerhem <guillaumekeerhem@MacBook-Pro-de-Guillaume.local>
2022-03-29 09:59:18 +0200

    Added tsDoc

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,11 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { Main } from './components/Main';
 
+/**
+ * App components that will be use to load the site
+ *
+ * @returns {React.ReactElement} Display the Main component
+ */
 export const App = () => {
   return (
     <BrowserRouter>
