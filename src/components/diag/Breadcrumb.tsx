42259e9e228ea94b327a7624ef0d8bf04f68e924
Guillaume Keerhem <guillaumekeerhem@mbpdeguillaume.lille.fr.ibm.com>
2022-03-30 10:26:14 +0200

    Made a breadcrumb to help the user through navigation

---
+++ src/components/diag/Breadcrumb.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { SBreadcrumb } from '~components/diag/SBreadcrumb';
+
+interface BreadcrumbProps {
+  navGuide: string;
+}
+
+/**
+ * Breadcrumb to indicate the users at what step they are in the experience
+ *
+ * @param {string} navGuide current step of the experience
+ * @returns {React.ReactElement} display the current step
+ */
+export const Breadcrumb = ({ navGuide }: BreadcrumbProps) => {
+  return <SBreadcrumb>{navGuide}</SBreadcrumb>;
+};
---
+++ src/components/diag/SBreadcrumb.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const SBreadcrumb = styled.p`
+  font-size: 11px;
+  line-height: 16px;
+  letter-spacing: 0.5px;
+  text-transform: lowercase;
+  color: var(--main-color);
+  margin-left: 60px;
+  align-self: flex-start;
+`;
