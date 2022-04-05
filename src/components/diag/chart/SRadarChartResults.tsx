96e4ec4073e378e81ef08d5f89f3117e9b84f4d7
Guillaume Keerhem <guillaumekeerhem@MacBook-Pro-de-Guillaume.local>
2022-04-05 16:25:19 +0200

    Made the components to display the spider chart

---
+++ src/components/diag/chart/SRadarChartResults.tsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const SRadarChartResults = styled.div`
+  position: relative;
+  margin: 0 0 0 5em;
+
+  .chartBg {
+    position: absolute;
+    top: 20%;
+    left: 21.7%;
+  }
+`;
