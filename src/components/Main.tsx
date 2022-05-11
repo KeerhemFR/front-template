4c7d1dfe1d22607664f661e7dfe4eaf6f869ee8c
Guillaume Keerhem <guillaumekeerhem@mbpdeguillaume.lille.fr.ibm.com>
2022-05-11 11:29:36 +0200

    Made protected route that will not display if the user is not authenticated

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,8 +1,30 @@
-import React from 'react';
+import React, { createContext, useContext, useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
+import { Login } from '~components/auth/Login';
 import { DiagResult } from '~components/diag/DiagResult';
 import { HeaderMenu } from '~components/header/HeaderMenu';
 import { StarterScreen } from '~components/starterScreen/StarterScreen';
+import { ProtectedRoute } from '~components/auth/ProtectedRoute';
+
+type AuthenticatedContext = [
+  boolean,
+  React.Dispatch<React.SetStateAction<boolean>>
+];
+
+const AUTHENTICATED_CONTEXT = createContext<AuthenticatedContext>([
+  false,
+  (() => {}) as React.Dispatch<React.SetStateAction<boolean>>,
+]);
+
+export function useAuthenticate(): AuthenticatedContext {
+  const ctx = useContext(AUTHENTICATED_CONTEXT);
+
+  if (!ctx) {
+    throw new Error('Context cannot be used outside boundaries');
+  }
+
+  return ctx;
+}
 
 /**
  * Main component that display header, footer and differents pages depending the path
@@ -10,13 +32,22 @@ import { StarterScreen } from '~components/starterScreen/StarterScreen';
  * @returns {React.ReactElement} display headers, footer, and pages components depending the path
  */
 export const Main = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
+
   return (
     <React.Fragment>
-      <HeaderMenu />
-      <Routes>
-        <Route path="/" element={<StarterScreen />} />
-        <Route path="/diagResult" element={<DiagResult />} />
-      </Routes>
+      <AUTHENTICATED_CONTEXT.Provider
+        value={[isAuthenticated, setIsAuthenticated]}
+      >
+        <HeaderMenu />
+        <Routes>
+          <Route path="/" element={<StarterScreen />} />
+          <Route path="/login" element={<Login />} />
+          <Route element={<ProtectedRoute />}>
+            <Route path="/diagResult" element={<DiagResult />} />
+          </Route>
+        </Routes>
+      </AUTHENTICATED_CONTEXT.Provider>
     </React.Fragment>
   );
 };
