1c0ddce3269af30f06c959d86a6761e11fe254ac
Guillaume Keerhem <guillaumekeerhem@mbpdeguillaume.lille.fr.ibm.com>
2022-05-18 11:23:57 +0200

    Changed the value of the context to an object for easiest use

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -8,17 +8,17 @@ import { ProtectedRoute } from '~components/auth/ProtectedRoute';
 import { TokenRoute } from '~components/auth/TokenRoute';
 
 type AuthenticatedContext = [
-  boolean,
-  React.Dispatch<React.SetStateAction<boolean>>,
-  string,
-  React.Dispatch<React.SetStateAction<string>>
+  { isAuthenticated: boolean; userName: string },
+  React.Dispatch<
+    React.SetStateAction<{ isAuthenticated: boolean; userName: string }>
+  >
 ];
 
 const AUTHENTICATED_CONTEXT = createContext<AuthenticatedContext>([
-  false,
-  (() => {}) as React.Dispatch<React.SetStateAction<boolean>>,
-  '',
-  (() => {}) as React.Dispatch<React.SetStateAction<string>>,
+  { isAuthenticated: false, userName: '' },
+  (() => {}) as React.Dispatch<
+    React.SetStateAction<{ isAuthenticated: boolean; userName: string }>
+  >,
 ]);
 
 /**
@@ -42,14 +42,19 @@ export function useAuthenticate(): AuthenticatedContext {
 export const Main: React.FunctionComponent = React.memo(() => {
   Main.displayName = 'Main';
 
-  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
-  const [userName, setUserName] = useState<string>('');
+  //const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
+  //const [userName, setUserName] = useState<string>('');
+  const [authData, setAuthData] = useState<{
+    isAuthenticated: boolean;
+    userName: string;
+  }>({
+    isAuthenticated: false,
+    userName: '',
+  });
 
   return (
     <React.Fragment>
-      <AUTHENTICATED_CONTEXT.Provider
-        value={[isAuthenticated, setIsAuthenticated, userName, setUserName]}
-      >
+      <AUTHENTICATED_CONTEXT.Provider value={[authData, setAuthData]}>
         <HeaderMenu />
         <Routes>
           <Route element={<TokenRoute />}>
